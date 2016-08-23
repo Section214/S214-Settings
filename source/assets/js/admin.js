688ea8c4e3e2a2942c422991ed57d7e30251cb37
Ghost1227 <dgriffiths@section214.com>
2016-08-23 03:43:13 -0500

    Update JS

--- source/assets/js/admin.js
+++ source/assets/js/admin.js
@@ -1,48 +1,37 @@
-/*global jQuery, document, window, wp, _wpMediaViewsL10n, file_frame, CodeMirror, s214_settings_vars*/
+/*global jQuery, document, window, wp, CodeMirror, s214_settings_vars*/
 jQuery(document).ready(function ($) {
-    "use strict";
-
-    // Icon isn't valid for modals!
-    $("select[name='" + s214_settings_vars.func + "_settings[display_type]']").change(function () {
-        var selectedItem = $("select[name='" + s214_settings_vars.func + "_settings[display_type]'] option:selected");
-
-        if (selectedItem.val() === "popover") {
-            $("select[name='" + s214_settings_vars.func + "_settings[icon]']").closest("tr").css("display", "table-row");
-        } else {
-            $("select[name='" + s214_settings_vars.func + "_settings[icon]']").closest("tr").css("display", "none");
-        }
-    }).change();
+    'use strict';
 
     // Setup color picker
-    if ($(".s214-color-picker").length) {
-        $(".s214-color-picker").wpColorPicker();
+    if ($('.s214-color-picker').length) {
+        $('.s214-color-picker').wpColorPicker();
     }
 
     // Setup select2
-    if ($(".s214-select2").length) {
-        $(".s214-select2").select2();
+    if ($('.s214-select2').length) {
+        $('.s214-select2').select2();
     }
 
     // Setup CodeMirror
-    if ($(".s214-html").length) {
-        $(".s214-html").each(function (index, elem) {
+    if ($('.s214-html').length) {
+        $('.s214-html').each(function (index, elem) {
             CodeMirror.fromTextArea(elem, {
                 lineNumbers: true,
-                mode: "text/html",
+                mode: 'text/html',
                 showCursorWhenSelecting: true
             });
         });
     }
 
     // Setup tooltips
-    $(".s214-help-tip").tooltip({
+    $('.s214-help-tip').tooltip({
         content: function () {
-            return $(this).prop("title");
+            return $(this).prop('title');
         },
         position: {
-            my: "center top",
-            at: "center bottom+10",
-            collision: "flipfit"
+            my: 'center top',
+            at: 'center bottom+10',
+            collision: 'flipfit'
         },
         hide: {
             duration: 200
@@ -53,10 +42,10 @@ jQuery(document).ready(function ($) {
     });
 
     // Setup uploaders
-    if ($("." + s214_settings_vars.func + "_settings_upload_button").length) {
+    if ($('.' + s214_settings_vars.func + '_settings_upload_button').length) {
         var file_frame;
 
-        $("body").on("click", "." + s214_settings_vars.func + "_settings_upload_button", function (e) {
+        $('body').on('click', '.' + s214_settings_vars.func + '_settings_upload_button', function (e) {
             e.preventDefault();
 
             var button = $(this);
@@ -71,36 +60,36 @@ jQuery(document).ready(function ($) {
 
             // Create the media frame
             wp.media.frames.file_frame = wp.media({
-                frame: "post",
-                state: "insert",
-                title: button.data("uploader_title"),
+                frame: 'post',
+                state: 'insert',
+                title: button.data('uploader_title'),
                 button: {
-                    text: button.data("uploader_button_text")
+                    text: button.data('uploader_button_text')
                 },
                 multiple: false
             });
 
             file_frame = wp.media.frames.file_frame;
 
-            file_frame.on("menu:render:default", function (view) {
+            file_frame.on('menu:render:default', function (view) {
                 // Store our views in an object
                 var views = {};
 
                 // Unset default menu items
-                view.unset("library-separator");
-                view.unset("gallery");
-                view.unset("featured-image");
-                view.unset("embed");
+                view.unset('library-separator');
+                view.unset('gallery');
+                view.unset('featured-image');
+                view.unset('embed');
 
                 // Initialize the views in our object
                 view.set(views);
             });
 
             // Run a callback on select
-            file_frame.on("insert", function () {
-                var selection = file_frame.state().get("selection");
+            file_frame.on('insert', function () {
+                var selection = file_frame.state().get('selection');
 
-                selection.each(function (attachment, index) {
+                selection.each(function (attachment) {
                     attachment = attachment.toJSON();
                     window.formfield.val(attachment.url);
                 });
@@ -110,6 +99,6 @@ jQuery(document).ready(function ($) {
             file_frame.open();
         });
 
-        window.formfield = "";
+        window.formfield = '';
     }
 });
